1c8705f3ff6328d81f448bf63319e2f990edca48
Aizu <149680681+Aizu-x@users.noreply.github.com>
2024-09-21 20:58:02 +0300

    Add files via upload

---
+++ elephant.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int location=0; // location of elephant.
+
+    cin>>location; // user input.
+
+    cout<<((location/5)+(location%5!=0?+1:+0)); // location divided by 5 (steps) and + if 5 is not enough.
+
+    return 0;
+}
